03be7fba287172cf630b3a0a21ec242f93574856
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-03 19:57:34 +0900

    a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,3 +1,14 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        x: i32,
+        y: i32,
+    }
+
+    let mut ans = 0;
+
+    while x + (ans * 10) < y {
+        ans += 1;
+    }
+
+    println!("{}", ans);
 }
