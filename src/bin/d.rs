66125b1e77a436d01f8d032e6fa56ae2bb9dfabb
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-04 16:00:28 +0900

    d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,3 +1,17 @@
+use itertools::Itertools;
+
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: usize,
+        k: i64,
+        a: [i64; n],
+    }
+
+    let ans = (0..n).combinations_with_replacement(2)
+                    .into_iter()
+                    .map(|comb| (comb[0], comb[1]))
+                    .map(|(l, r)| (&a[l..=r]).iter().sum::<i64>())
+                    .fold(0, |count, ans| count + if k == ans {1} else {0});
+
+    println!("{}", ans);
 }
