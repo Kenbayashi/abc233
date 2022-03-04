4901ed6972e43405167064897705ede6e53cf826
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-04 11:38:47 +0900

    b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        l: usize,
+        r: usize,
+        s: Chars,
+    }
+
+    let length = s.len();
+
+    for count in 1..=length {
+        if (l <= count) && (count <= r) {
+            // 内側
+            print!("{}", s[r - count + l - 1]);
+        } else {
+            // 外側
+            print!("{}", s[count - 1]);
+        }
+    }
 }
